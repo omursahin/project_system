75f117de0a3e5298afb43d3145f508ac6c482d4f
Oguzhan <ogzhn.cnr0@gmail.com>
2023-04-15 17:14:01 +0300

    Edit semester_course model.py
    Added requested features to the Semester Course model

--- semester_course/models.py
+++ semester_course/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from course.models import Course
+from semester.models import Semester
 
-# Create your models here.
+
+class SemesterCourse(models.Model):
+    semester = models.ForeignKey(Semester, on_delete=models.CASCADE, unique=True, null=False, blank=True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, unique=True, null=False, blank=True)
+    max_group_size = models.IntegerField(null=False, blank=True)
+
+    class Meta:
+        db_table = 'semester_course'
+        verbose_name = 'SemesterCourse'
+        verbose_name_plural = 'SemesterCourses'
+        unique_together = ('semester', 'course',)
+
+    def _str_(self):
+        return self.semester + " " + self.course
