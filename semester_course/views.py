10962dd5c88dacfe36557c3d9178492413986f87
yufus7 <79915956+yufus7@users.noreply.github.com>
2023-04-16 16:04:01 +0300

    edit semester_course/views.py

--- semester_course/views.py
+++ semester_course/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+
+from semester_course.models import SemesterCourse
+from semester_course.serializers import SemesterCourseSerializer
+from project_system.permissions import ReadOnly
+
 
 # Create your views here.
+class SemesterCourseList(generics.ListCreateAPIView):
+    permission_classes = [IsAdminUser | ReadOnly]
+    queryset = SemesterCourse.objects.all()
+    serializer_class = SemesterCourseSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class SemesterCourseDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated | ReadOnly]
+    queryset = SemesterCourse.objects.all()
+    serializer_class = SemesterCourseSerializer
