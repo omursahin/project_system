4a8be9beaa9f80aeff1e0c9367b0474a2e174c3a
fatihyavan <fatih.yavan@outlook.com>
2023-04-18 13:16:16 +0300

    Fixing model of blank and views.py functions

--- semester_course/models.py
+++ semester_course/models.py
@@ -5,10 +5,10 @@ from semester.models import Semester
 
 class SemesterCourse(models.Model):
     semester = models.ForeignKey(Semester, on_delete=models.CASCADE,
-                                 null=False, blank=True)
+                                 null=False, blank=False)
     course = models.ForeignKey(Course, on_delete=models.CASCADE,
-                               null=False, blank=True)
-    max_group_size = models.IntegerField(null=False, blank=True)
+                               null=False, blank=False)
+    max_group_size = models.IntegerField(null=False, blank=False)
 
     class Meta:
         db_table = 'semester_course'
