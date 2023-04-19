6afdbc4d588bfeac4e66b9d99a20cc949eeb349a
Omur <1030520002@erciyes.edu.tr>
2023-04-19 16:19:53 +0300

    Serializer detaylandırma

--- semester_course/serializers.py
+++ semester_course/serializers.py
@@ -1,9 +1,21 @@
 from rest_framework import serializers
 
+from course.serializers import CourseSerializer
+from semester.serializers import SemesterSerializer
 from semester_course.models import SemesterCourse
 
 
-class SemesterCourseSerializer(serializers.ModelSerializer):
+class SemesterCourseGetSerializer(serializers.ModelSerializer):
+    semester = SemesterSerializer()
+    course = CourseSerializer()
+
+    class Meta:
+        model = SemesterCourse
+        fields = '__all__'
+
+
+class SemesterCoursePostSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = SemesterCourse
         fields = '__all__'
