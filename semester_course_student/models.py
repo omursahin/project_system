dc61038f99ed921dcba9a080ff3f4b814595607d
muratkaplan06 <muratkaplan061989@gmail.com>
2023-04-20 21:09:13 +0300

    semester_course_student
    semester_course_student is created

--- semester_course_student/models.py
+++ semester_course_student/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class SemesterCourseStudent(models.Model):
+    semester_course = models.ForeignKey('semester_course.SemesterCourse', on_delete=models.CASCADE,
+                                        null=False, blank=False)
+    student = models.ForeignKey('account.MyUser', on_delete=models.CASCADE,
+                                null=False, blank=False)
+    mid_term = models.IntegerField(null=False, blank=False)
+    final = models.IntegerField(null=False, blank=False)
