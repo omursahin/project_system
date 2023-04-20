cd4b8a1af1a5420373bb70fdb4c72b6ebbec2064
muratkaplan06 <muratkaplan061989@gmail.com>
2023-04-20 21:37:09 +0300

    view is created
    permission is created

--- semester_course_student/views.py
+++ semester_course_student/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAdminUser
+
+from semester_course_student.models import SemesterCourseStudent
+from semester_course_student.serializers import SemesterCourseStudentSerializer
+from project_system.permissions import ReadOnly
+
 
 # Create your views here.
+class SemesterCourseStudentList(generics.ListCreateAPIView):
+    permission_classes = [IsAdminUser | ReadOnly]
+    queryset = SemesterCourseStudent.objects.all()
+    serializer_class = SemesterCourseStudentSerializer
