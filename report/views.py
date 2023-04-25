2512966dee6c1c2e64f47b09cae309eeed827735
Azad YILDIZ <azad.yildiz119@gmail.com>
2023-04-25 13:21:06 +0300

    Added views

--- report/views.py
+++ report/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
+from report.models import Report
+from report.serializers import ReportSerializer
+from project_system.permissions import ReadOnly
+
 
 # Create your views here.
+class ReportList(generics.ListCreateAPIView):
+    permission_classes = [IsAdminUser | ReadOnly]
+    queryset = Report.objects.all()
+    serializer_class = ReportSerializer
+
+
+class ReportDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated | ReadOnly]
+    queryset = Report.objects.all()
+    serializer_class = ReportSerializer
