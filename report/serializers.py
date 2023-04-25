514584eba1ed06ef6e03ff992c74edae4d706900
Ahmet Buğra Erbaş <ahmetbgr2@gmail.com>
2023-04-25 22:22:19 +0300

    Updated views and serializers

--- report/serializers.py
+++ report/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 
 from report.models import Report
+from semester_course.serializers import SemesterCourseGetSerializer
 
 
-class ReportSerializer(serializers.ModelSerializer):
+class ReportGetSerializer(serializers.ModelSerializer):
+    semester_course = SemesterCourseGetSerializer()
+
+    class Meta:
+        model = Report
+        fields = '__all__'
+
+
+class ReportPostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Report
         fields = '__all__'
--- report/views.py
+++ report/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 from report.models import Report
-from report.serializers import ReportSerializer
+from report.serializers import ReportGetSerializer, ReportPostSerializer
 from project_system.permissions import ReadOnly
 
 
@@ -10,10 +10,19 @@ from project_system.permissions import ReadOnly
 class ReportList(generics.ListCreateAPIView):
     permission_classes = [IsAdminUser | ReadOnly]
     queryset = Report.objects.all()
-    serializer_class = ReportSerializer
+
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return ReportGetSerializer
+        return ReportPostSerializer
 
 
 class ReportDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthenticated | ReadOnly]
     queryset = Report.objects.all()
-    serializer_class = ReportSerializer
+    serializer_class = ReportGetSerializer
+
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return ReportGetSerializer
+        return ReportPostSerializer
