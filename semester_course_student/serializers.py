35b47096bcb83317ade17ce645f3f419d51bd973
hafizegungor <1030520844@erciyes.edu.tr>
2023-04-25 19:58:21 +0300

    views.py and serializer.py update

--- semester_course_student/serializers.py
+++ semester_course_student/serializers.py
@@ -1,9 +1,19 @@
 from rest_framework import serializers
 
+from project_system.urls import UserSerializer
+from semester_course.serializers import SemesterCourseGetSerializer
 from semester_course_student.models import SemesterCourseStudent
 
+class SemesterCourseStudentGetSerializer(serializers.ModelSerializer):
+    semester_course = SemesterCourseGetSerializer()
+    student = UserSerializer()
 
-class SemesterCourseStudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SemesterCourseStudent
+        fields = '__all__'
+
+
+class SemesterCourseStudentPostSerializer(serializers.ModelSerializer):
     class Meta:
         model = SemesterCourseStudent
         fields = '__all__'
--- semester_course_student/views.py
+++ semester_course_student/views.py
@@ -1,19 +1,29 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated, IsStaff
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 from semester_course_student.models import SemesterCourseStudent
-from semester_course_student.serializers import SemesterCourseStudentSerializer
+from semester_course_student.serializers \
+    import SemesterCourseStudentGetSerializer, SemesterCourseStudentPostSerializer
 from project_system.permissions import ReadOnly
 
 
 # Create your views here.
 class SemesterCourseStudentList(generics.ListCreateAPIView):
-    permission_classes = [IsAuthenticated | IsStaff | ReadOnly]
+    permission_classes = [IsAdminUser | ReadOnly]
     queryset = SemesterCourseStudent.objects.all()
-    serializer_class = SemesterCourseStudentSerializer
+
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return SemesterCourseStudentGetSerializer
+        return SemesterCourseStudentPostSerializer
 
 
 class SemesterCourseStudentDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated | IsStaff | ReadOnly]
+    permission_classes = [IsAuthenticated]
     queryset = SemesterCourseStudent.objects.all()
-    serializer_class = SemesterCourseStudentSerializer
+    serializer_class = SemesterCourseStudentGetSerializer
+
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return SemesterCourseStudentGetSerializer
+        return SemesterCourseStudentPostSerializer
