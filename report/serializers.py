196c637cfb2b7e74e64c280ad95fa4e17ef7406a
Ahmet Buğra Erbaş <ahmetbgr2@gmail.com>
2023-04-25 13:15:50 +0300

    Added serializers

---
+++ report/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from report.models import Report
+
+
+class ReportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Report
+        fields = '__all__'
+        
