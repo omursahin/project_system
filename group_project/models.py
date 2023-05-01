46378acd617da43c6e994ae0fc0fbe7ddcf57239
Oguzhan <ogzhn.cnr0@gmail.com>
2023-05-02 01:01:50 +0300

    Edit model

--- group_project/models.py
+++ group_project/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from group.models import Group
 
-# Create your models here.
+
+class GroupProject(models.Model):
+    STATUS_CHOICES = (
+        ('A', 'Approved'),
+        ('P', 'Pending'),
+        ('D', 'Draft'),
+        ('R', 'Rejected'),
+    )
+
+    group = models.ForeignKey(
+        'group.Group',
+        on_delete=models.CASCADE, related_name='group')
+    title = models.CharField(max_length=255, blank=False, null=False)
+    description = models.CharField(max_length=255, blank=True, null=True)
+    is_approved = models.BooleanField()
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+
+    class Meta:
+        db_table = 'group_project'
+        verbose_name = 'GroupProject'
+        verbose_name_plural = 'GroupProjects'
+
+    def __str__(self):
+        return self.title
