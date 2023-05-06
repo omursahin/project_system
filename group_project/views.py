9f6295b9c30d4f8f7f25ed656badd15a763a542c
fatihyavan <fatih.yavan@outlook.com>
2023-05-06 15:02:21 +0300

    added views.py

--- group_project/views.py
+++ group_project/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
+from group_project.models import GroupProject
+from group_project.serializers import GroupProjectSerializer
+from project_system.permissions import ReadOnly
+
 
 # Create your views here.
+class GroupProjectList(generics.ListCreateAPIView):
+    permission_classes = [IsAdminUser | ReadOnly]
+    queryset = GroupProject.objects.all()
+    serializer_class = GroupProjectSerializer
+
+
+class GroupProjectDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated | ReadOnly]
+    queryset = GroupProject.objects.all()
+    serializer_class = GroupProjectSerializer
