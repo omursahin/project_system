db7504d98cb0dbb31e65d57d4502323d21af3226
hafizegungor <1030520844@erciyes.edu.tr>
2023-05-21 16:26:04 +0300

    models.py created

--- project_report/models.py
+++ project_report/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class ProjectReport(models.Model):
+    project = models.ForeignKey('group_project.GroupProject',
+                                        on_delete=models.CASCADE,
+                                        null=False, blank=False)
+    report = models.ForeignKey('report.Report', on_delete=models.CASCADE,
+                                null=False, blank=False)
+
+    description = models.CharField(null=True, blank=True)
+    file = models.FileField(null=True, blank=True)
+    is_submitted = models.BooleanField
+    version = models.IntegerField
+    plagiarism_file = models.FileField(null=True, blank=True)
+    plagiarism_rate = models.IntegerField(null=True, blank=True)
